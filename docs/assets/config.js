dbd1271f0c83772881b4effd541022a3bfac29aa
Kenmochi Hiroki <e225408@ie.u-ryukyu.ac.jp>
2026-04-07 08:39:23 +0900

    Configure GAS URL and site settings for GitHub Pages

--- docs/assets/config.js
+++ docs/assets/config.js
@@ -1,7 +1,7 @@
 window.BIJIRIS_CONFIG = Object.assign({}, window.BIJIRIS_CONFIG || {}, {
   apiMode: "gas",
-  gasUrl: "",
-  siteRootUrl: "",
-  respondentUrl: "",
-  adminUrl: ""
+  gasUrl: "https://script.google.com/macros/s/AKfycbxjaZUpoenBPzbYit9vpgsXvC5Oo74PCie4yCH2CMeQ5gJ27Be6hlkh95J6UfhLnt1jKQ/exec",
+  siteRootUrl: "https://mayumi-josanin.github.io/bijiris/",
+  respondentUrl: "https://mayumi-josanin.github.io/bijiris/",
+  adminUrl: "https://mayumi-josanin.github.io/bijiris/admin/"
 });
